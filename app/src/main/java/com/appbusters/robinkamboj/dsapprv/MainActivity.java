f0160f124610ec754d9fef301f9e83365e7dcbe5
Robin Kamboj <dev.robinkamboj@gmail.com>
2017-04-26 21:09:48 +0530

    RV.

--- app/src/main/java/com/appbusters/robinkamboj/dsapprv/MainActivity.java
+++ app/src/main/java/com/appbusters/robinkamboj/dsapprv/MainActivity.java
@@ -2,16 +2,25 @@ package com.appbusters.robinkamboj.dsapprv;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
 
     private String[] allInterests, diet, yoga, health, religion, motivation, ayurveda, astrology;
     private int[] allColors;
+    private RecyclerView recyclerView;
+    private GridLayoutManager gridLayoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerview);
+        gridLayoutManager = new GridLayoutManager(getApplicationContext(), 3);
+        recyclerView.setLayoutManager(gridLayoutManager);
+
 
         allInterests = getResources().getStringArray(R.array.allInterests);
         allColors = getResources().getIntArray(R.array.rvItemColors);
