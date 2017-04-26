55ea56550ecc8839418fdb3293eab06ebad3b5ba
Robin Kamboj <dev.robinkamboj@gmail.com>
2017-04-26 22:27:59 +0530

    RV.

--- app/src/main/java/com/appbusters/robinkamboj/dsapprv/controller/RecyclerViewAdapter.java
+++ app/src/main/java/com/appbusters/robinkamboj/dsapprv/controller/RecyclerViewAdapter.java
@@ -1,4 +1,46 @@
 package com.appbusters.robinkamboj.dsapprv.controller;
 
-public class RecyclerViewAdapter {
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.appbusters.robinkamboj.dsapprv.R;
+import com.appbusters.robinkamboj.dsapprv.model.Data;
+import com.appbusters.robinkamboj.dsapprv.model.View_Holder;
+import com.bumptech.glide.Glide;
+
+import java.util.Collections;
+import java.util.List;
+
+public class RecyclerViewAdapter extends RecyclerView.Adapter<View_Holder>{
+
+    private Context context;
+    private int[] drawables;
+    private List<Data> list = Collections.emptyList();
+
+    public RecyclerViewAdapter(Context context, List<Data> list ) {
+        this.context = context;
+        this.list = list;
+    }
+
+    @Override
+    public View_Holder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(context).inflate(R.layout.row_layout, parent, false);
+        return new View_Holder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(View_Holder holder, int position) {
+        Glide.with(context)
+                .load(list.get(position).getDrawable())
+                .into(holder.drawable);
+        holder.header.setText(list.get(position).getHeader());
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
 }
--- app/src/main/java/com/appbusters/robinkamboj/dsapprv/view/MainActivity.java
+++ app/src/main/java/com/appbusters/robinkamboj/dsapprv/view/MainActivity.java
@@ -6,24 +6,27 @@ import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
 import com.appbusters.robinkamboj.dsapprv.R;
+import com.appbusters.robinkamboj.dsapprv.controller.RecyclerViewAdapter;
+import com.appbusters.robinkamboj.dsapprv.model.Data;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private String[] allInterests, diet, yoga, health, religion, motivation, ayurveda, astrology;
-    private int[] allColors;
+    private int[] allColors, allDrawables;
     private RecyclerView recyclerView;
     private GridLayoutManager gridLayoutManager;
+    private List<Data> data;
+    private RecyclerViewAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        recyclerView = (RecyclerView) findViewById(R.id.recyclerview);
-        gridLayoutManager = new GridLayoutManager(getApplicationContext(), 3);
-        recyclerView.setLayoutManager(gridLayoutManager);
-
-
+        allDrawables = new int[]{R.drawable.ic_diet, R.drawable.ic_yoga, R.drawable.ic_health, R.drawable.ic_god, R.drawable.ic_motivation, R.drawable.ic_peace, R.drawable.ic_astrology};
         allInterests = getResources().getStringArray(R.array.allInterests);
         allColors = getResources().getIntArray(R.array.rvItemColors);
         diet = getResources().getStringArray(R.array.diet_array);
@@ -33,5 +36,21 @@ public class MainActivity extends AppCompatActivity {
         motivation = getResources().getStringArray(R.array.motivation_array);
         ayurveda = getResources().getStringArray(R.array.ayurveda_array);
         astrology = getResources().getStringArray(R.array.astrology_array);
+
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerview);
+        gridLayoutManager = new GridLayoutManager(getApplicationContext(), 3);
+        recyclerView.setLayoutManager(gridLayoutManager);
+
+        data = fillWithData();
+        adapter = new RecyclerViewAdapter(getApplicationContext(), data);
+        recyclerView.setAdapter(adapter);
+    }
+
+    private List<Data> fillWithData(){
+        List<Data> data = new ArrayList<>();
+        for(int i = 0; i < allInterests.length; i++){
+            data.add(new Data(allInterests[i], allDrawables[i], i+1));
+        }
+        return data;
     }
 }
